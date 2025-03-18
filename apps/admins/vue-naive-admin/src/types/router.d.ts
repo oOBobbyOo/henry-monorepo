38b08653a0b89a6ae050f0af1ecc471faa079814
oOBobbyOo <944831575@qq.com>
2025-03-18 14:57:07 +0800

    feat: dynamicRoutes

--- apps/admins/vue-naive-admin/src/types/router.d.ts
+++ apps/admins/vue-naive-admin/src/types/router.d.ts
@@ -1,1 +1,9 @@
+import type { RouteRecordRaw } from 'vue-router'
+
 export type IRouterMap = 'web' | 'hash' | 'memory'
+
+export type IRouteRecordRaw = RouteRecordRaw
+
+export interface IModuleRaw {
+  default: Array<RouteRecordRaw> | RouteRecordRaw
+}
