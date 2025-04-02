0968155416fb449b8e980af4588aac1b1c9d3a28
oOBobbyOo <944831575@qq.com>
2025-04-02 10:22:17 +0800

    feat: add SiderToggler

--- apps/admins/vue-naive-admin/src/layouts/modules/global-header/index.vue
+++ apps/admins/vue-naive-admin/src/layouts/modules/global-header/index.vue
@@ -1,15 +1,22 @@
 <script setup lang='ts'>
+import { useAppStore } from '@/stores/modules/app'
 import { useThemeStore } from '@/stores/modules/theme'
+
+import DarkMode from './components/DarkMode/index.vue'
+import SiderToggler from './components/SiderToggler/index.vue'
 
 defineOptions({ name: 'GlobalHeader' })
 
+const appStore = useAppStore()
 const themeStore = useThemeStore()
 </script>
 
 <template>
   <div class="layout-header flex-between px-3">
-    <div>1</div>
-    <DarkMode :theme-schema="themeStore.themeSchema" />
+    <SiderToggler :collapsed="appStore.siderCollapse" @click="appStore.toggleSiderCollapse" />
+    <div class="h-full flex-y-center justify-end">
+      <DarkMode :theme-schema="themeStore.themeSchema" />
+    </div>
   </div>
 </template>
 
