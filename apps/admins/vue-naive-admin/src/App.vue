5582a3d28f25047091f505e5fc08b7734e72c56b
oOBobbyOo <944831575@qq.com>
2025-04-03 16:14:33 +0800

    refactor: change App provider

--- apps/admins/vue-naive-admin/src/App.vue
+++ apps/admins/vue-naive-admin/src/App.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
+import { naiveDateLocales, naiveLocales } from '@/locales/naive'
+import { useAppStore } from '@/stores/modules/app'
 import { useThemeStore } from '@/stores/modules/theme'
 import { darkTheme } from 'naive-ui'
 import { computed } from 'vue'
 
+const appStore = useAppStore()
 const themeStore = useThemeStore()
 
 const naiveDarkTheme = computed(() => (themeStore.darkMode ? darkTheme : undefined))
+
+const naiveLocale = computed(() => {
+  return naiveLocales[appStore.locale]
+})
+
+const naiveDateLocale = computed(() => {
+  return naiveDateLocales[appStore.locale]
+})
 </script>
 
 <template>
-  <n-config-provider :theme="naiveDarkTheme" class="wh-full">
+  <n-config-provider
+    :theme="naiveDarkTheme"
+    :locale="naiveLocale"
+    :date-locale="naiveDateLocale"
+    class="wh-full"
+  >
     <router-view />
   </n-config-provider>
 </template>
