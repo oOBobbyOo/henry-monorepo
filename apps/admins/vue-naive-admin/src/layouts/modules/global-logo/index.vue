11975f56aad105f1391a2b62c71e5b33adcb1702
oOBobbyOo <944831575@qq.com>
2025-04-03 18:22:50 +0800

    refactor: change global-logo

--- apps/admins/vue-naive-admin/src/layouts/modules/global-logo/index.vue
+++ apps/admins/vue-naive-admin/src/layouts/modules/global-logo/index.vue
@@ -1,11 +1,42 @@
 <script setup lang='ts'>
+import { useAppStore } from '@/stores/modules/app'
+
 defineOptions({ name: 'GlobalLogo' })
+
+const appStore = useAppStore()
 </script>
 
 <template>
-  <div>global-logo</div>
+  <div flex-center p-4>
+    <h1 className="logo">
+      {{ appStore.siderCollapse ? 'HA' : 'Heny Admin' }}
+    </h1>
+  </div>
 </template>
 
 <style scoped>
+.logo {
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 28px;
+  text-align: center;
+  white-space: nowrap;
+  list-style-type: none;
+  background-image: linear-gradient(to right, #0a98d2, #1ffdc4, #0a98d2, orange, #0a98d2, #1ffdc4, #0a98d2);
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  background-size: 200% 100%;
+  animation: bgp 8s infinite linear;
+}
 
+@keyframes bgp {
+  0% {
+    background-position: 0 0;
+  }
+
+  100% {
+    background-position: -100% 0;
+  }
+}
 </style>
