d26ecc63ccd302bc39a489c7afee6bb89d3e5004
oOBobbyOo <944831575@qq.com>
2025-04-14 14:48:57 +0800

    chore: path alias

--- apps/admins/react-antd-admin/vite.config.ts
+++ apps/admins/react-antd-admin/vite.config.ts
@@ -1,7 +1,18 @@
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./', import.meta.url)),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    plugins: [
+      react(),
+    ],
+  }
 })
