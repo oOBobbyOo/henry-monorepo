759dc8e56e365f0afe76f74a0def5e9f8de1346f
oOBobbyOo <944831575@qq.com>
2025-04-24 16:16:18 +0800

    refactor: about

--- apps/admins/vue-naive-admin/src/views/about/index.vue
+++ apps/admins/vue-naive-admin/src/views/about/index.vue
@@ -1,9 +1,45 @@
 <script setup lang='ts'>
-
+import pkg from '~/package.json'
 </script>
 
 <template>
-  <div>about</div>
+  <div>
+    <Card title="关于">
+      HenryAdmin 是一个优雅且功能强大的后台管理模板，基于最新的前端技术栈，包括 Vue3, Vite5, TypeScript, Pinia 和 UnoCSS。
+    </Card>
+
+    <Card title="开发环境依赖">
+      <n-descriptions
+        label-placement="left"
+        bordered
+        :column="2"
+      >
+        <n-descriptions-item
+          v-for="(item, key) in pkg.devDependencies"
+          :key="key"
+          :label="key"
+        >
+          {{ item }}
+        </n-descriptions-item>
+      </n-descriptions>
+    </Card>
+
+    <Card title="生产环境依赖">
+      <n-descriptions
+        label-placement="left"
+        bordered
+        :column="2"
+      >
+        <n-descriptions-item
+          v-for="(item, key) in pkg.dependencies"
+          :key="key"
+          :label="key"
+        >
+          {{ item }}
+        </n-descriptions-item>
+      </n-descriptions>
+    </Card>
+  </div>
 </template>
 
 <style scoped>
