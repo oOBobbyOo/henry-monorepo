6858da2accf2d30262e8d0b2f0b59cfa734e1c3c
oOBobbyOo <944831575@qq.com>
2025-04-24 16:15:21 +0800

    feat: add Card

---
+++ apps/admins/vue-naive-admin/src/components/Card/index.vue
@@ -0,0 +1,25 @@
+<script setup lang='ts'>
+import { useAttrs } from 'vue'
+
+defineOptions({ name: 'Card' })
+
+const attrs = useAttrs()
+</script>
+
+<template>
+  <n-card
+    size="small"
+    :bordered="false"
+    :segmented="{
+      content: true,
+    }"
+    class="b-rd-2"
+    v-bind="attrs"
+  >
+    <slot />
+  </n-card>
+</template>
+
+<style scoped>
+
+</style>
