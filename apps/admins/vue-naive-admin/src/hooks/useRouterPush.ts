7f706ecfcde2c4b17be639f2f982a1070193a677
oOBobbyOo <944831575@qq.com>
2025-04-27 12:17:36 +0800

    refactor: change useRouterPush

--- apps/admins/vue-naive-admin/src/hooks/useRouterPush.ts
+++ apps/admins/vue-naive-admin/src/hooks/useRouterPush.ts
@@ -4,6 +4,7 @@ import { useRouter } from 'vue-router'
 
 export function useRouterPush(inSetup = true) {
   const router = inSetup ? useRouter() : globalRouter
+  const route = globalRouter.currentRoute
 
   const routerPush = router.push
 
@@ -33,10 +34,67 @@ export function useRouterPush(inSetup = true) {
     return routerPushByKey(key, { query: meta?.query || {} })
   }
 
+  async function toHome() {
+    return routerPushByKey('root')
+  }
+
+  /**
+   * Navigate to login page
+   *
+   * @param loginModule The login module
+   * @param redirectUrl The redirect url, if not specified, it will be the current route fullPath
+   */
+  async function toLogin(loginModule?: Auth.LoginModuleKey, redirectUrl?: string) {
+    const module = loginModule || 'pwd-login'
+
+    const options: App.Global.RouterPushOptions = {
+      params: {
+        module,
+      },
+    }
+
+    const redirect = redirectUrl || route.value.fullPath
+
+    options.query = {
+      redirect,
+    }
+
+    return routerPushByKey('login', options)
+  }
+
+  /**
+   * Toggle login module
+   *
+   * @param module
+   */
+  async function toggleLoginModule(module: Auth.LoginModuleKey) {
+    const query = route.value.query as Record<string, string>
+
+    return routerPushByKey('login', { query, params: { module } })
+  }
+
+  /**
+   * Redirect from login
+   *
+   * @param [needRedirect] Whether to redirect after login. Default is `true`
+   */
+  async function redirectFromLogin(needRedirect = true) {
+    const redirect = route.value.query?.redirect as string
+
+    if (needRedirect && redirect) {
+      await routerPush(redirect)
+    }
+    else {
+      await toHome()
+    }
+  }
   return {
     routerPush,
     routerBack,
     routerPushByKey,
     routerPushByKeyWithMetaQuery,
+    toLogin,
+    toggleLoginModule,
+    redirectFromLogin,
   }
 }
