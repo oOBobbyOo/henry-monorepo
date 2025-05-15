ca6f8168983934e0a08fa030fb66327a1696f7ca
oOBobbyOo <944831575@qq.com>
2025-05-15 11:21:03 +0800

    refactor: initUserInfo

--- apps/admins/vue-naive-admin/src/stores/modules/user/index.ts
+++ apps/admins/vue-naive-admin/src/stores/modules/user/index.ts
@@ -1,14 +1,30 @@
+import { getUserInfo } from '@/api/user'
+import { getToken } from '@/utils'
+import { to } from '@henry/utils'
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
-  const userInfo: Api.UserInfo = reactive({
+  let userInfo: Api.UserInfo = reactive({
     userId: '',
     userName: '',
     avatar: '',
   })
 
+  async function initUserInfo() {
+    const hasToken = getToken()
+
+    if (hasToken) {
+      const [error, data] = await to(getUserInfo())
+
+      if (!error) {
+        userInfo = data
+      }
+    }
+  }
+
   return {
     userInfo,
+    initUserInfo,
   }
 })
