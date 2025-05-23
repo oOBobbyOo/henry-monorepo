40c791de37e656678f40c44b1de534db88bd3b4a
oOBobbyOo <944831575@qq.com>
2025-05-23 10:57:16 +0800

    chore: eslint ignores

--- configs/lint/eslint.config.mjs
+++ configs/lint/eslint.config.mjs
@@ -1,11 +1,18 @@
 // @see https://github.com/antfu/eslint-config
 import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    formatters: true,
-    typescript: true,
-    vue: true,
-    ignores: ['node_modules', 'dist', 'auto-imports.d.ts', 'compoents.d.ts'],
-  },
-)
+export default antfu({
+  formatters: true,
+  typescript: true,
+  vue: true,
+  ignores: [
+    'node_modules',
+    'dist',
+    '.turbo',
+    '.vscode',
+    '.nuxt',
+    '.output',
+    'auto-imports.d.ts',
+    'compoents.d.ts',
+  ],
+})
