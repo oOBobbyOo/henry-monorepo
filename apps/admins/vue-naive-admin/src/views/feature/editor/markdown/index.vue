5024e15be046b4037aa67cd424a3b5c1a01b889d
oOBobbyOo <944831575@qq.com>
2025-05-26 12:14:38 +0800

    refactor: change markdown

--- apps/admins/vue-naive-admin/src/views/feature/editor/markdown/index.vue
+++ apps/admins/vue-naive-admin/src/views/feature/editor/markdown/index.vue
@@ -1,9 +1,28 @@
 <script setup lang='ts'>
+// @see https://github.com/pd4d10/bytemd
+import gfm from '@bytemd/plugin-gfm'
+import { Editor } from '@bytemd/vue-next'
+import { ref } from 'vue'
+import 'bytemd/dist/index.css'
 
+const content = ref('# Hello, Markdown!')
+
+const plugins = [
+  gfm(),
+  // Add more plugins here
+]
+
+function handleChange(v: string) {
+  content.value = v
+}
 </script>
 
 <template>
-  <div>markdown</div>
+  <div>
+    <NCard title="Markdown" :bordered="false" size="small" segmented class="card-wrapper">
+      <Editor :value="content" :plugins="plugins" @change="handleChange" />
+    </NCard>
+  </div>
 </template>
 
 <style scoped>
