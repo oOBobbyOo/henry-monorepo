e1aa6e262833a3c5af42d20f7d2933a04309b139
oOBobbyOo <944831575@qq.com>
2025-06-16 14:47:28 +0800

    feat: add PaperLayerLoading

---
+++ apps/admins/vue-naive-admin/src/components/Animate/loading/PaperLayerLoading.vue
@@ -0,0 +1,44 @@
+<script setup lang='ts'>
+defineOptions({ name: 'PaperLayerLoading' })
+</script>
+
+<template>
+  <div class="paper-layer-loading" />
+</template>
+
+<style scoped>
+.paper-layer-loading {
+  position: relative;
+  width: 60px;
+  height: 60px;
+  margin: 0 auto;
+  background: #a19dad;
+  transform: rotateX(65deg) rotate(45deg);
+  color: #fff;
+  animation: layers1 1s linear infinite alternate;
+}
+.paper-layer-loading::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  background: rgba(255, 255, 255, 0.7);
+  animation: layerTr 1s linear infinite alternate;
+}
+@keyframes layers1 {
+  0% {
+    box-shadow: 0px 0px 0 0px;
+  }
+  90%,
+  100% {
+    box-shadow: 20px 20px 0 -4px;
+  }
+}
+@keyframes layerTr {
+  0% {
+    transform: translate(0, 0) scale(1);
+  }
+  100% {
+    transform: translate(-25px, -25px) scale(1);
+  }
+}
+</style>
--- apps/admins/vue-naive-admin/src/views/animate/loading/index.vue
+++ apps/admins/vue-naive-admin/src/views/animate/loading/index.vue
@@ -1,17 +1,23 @@
 <script setup lang='ts'>
 import BlockBounceLoading from '@/components/Animate/loading/BlcokBounceLoading.vue'
 import Block3DLoading from '@/components/Animate/loading/Block3DLoading.vue'
+import ImgLoading from '@/components/Animate/loading/ImgLoading.vue'
+import PaperLayerLoading from '@/components/Animate/loading/PaperLayerLoading.vue'
+import PinballLoading from '@/components/Animate/loading/PinballLoading.vue'
+import TextReversalLoading from '@/components/Animate/loading/TextReversalLoading.vue'
+import TextScrollLoading from '@/components/Animate/loading/TextScrollLoading.vue'
 </script>
 
 <template>
   <div class="loading m-10">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-20 gap-y-30 justify-center content-center">
-      <PinballLoading />
-      <ImgLoading />
-      <TextReversalLoading />
-      <TextScrollLoading />
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-30 justify-center content-center">
       <BlockBounceLoading />
       <Block3DLoading />
+      <ImgLoading />
+      <PaperLayerLoading />
+      <PinballLoading />
+      <TextReversalLoading />
+      <TextScrollLoading />
     </div>
   </div>
 </template>
