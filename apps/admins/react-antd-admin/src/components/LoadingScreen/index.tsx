53762945f56a97729a107442ca1790755b6edb79
oOBobbyOo <944831575@qq.com>
2025-07-01 11:00:59 +0800

    refactor: change LoadingScreen

--- apps/admins/react-antd-admin/src/components/LoadingScreen/index.tsx
+++ apps/admins/react-antd-admin/src/components/LoadingScreen/index.tsx
@@ -1,6 +1,28 @@
+import './index.css'
+
 function LoadingScreen() {
   return (
-    <div>LoadingScreen</div>
+    <div className="loading-screen">
+      <div className="loader">
+        <p>L</p>
+        <p>O</p>
+        <p>A</p>
+        <p>D</p>
+        <p>I</p>
+        <p>N</p>
+        <p>G</p>
+        <div className="arrows">
+          <div className="arrowsup">
+            <div className="arrow1"></div>
+            <div className="arrow2"></div>
+          </div>
+          <div className="arrowsbottom">
+            <div className="arrow3"></div>
+            <div className="arrow4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
