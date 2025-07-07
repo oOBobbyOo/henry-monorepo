ca235ed37117485c22af964ff61836002985e498
oOBobbyOo <944831575@qq.com>
2025-07-07 17:49:58 +0800

    feat: add SvgIcon

---
+++ apps/admins/react-antd-admin/src/components/SvgIcon/inde.tsx
@@ -0,0 +1,32 @@
+import type { FC } from 'react'
+import { Icon } from '@iconify/react'
+
+interface Props {
+  icon?: string
+}
+
+/** local svg */
+const localPrefix = 'local:'
+
+const SvgIcon: FC<Props> = ({ icon, ...rest }) => {
+  const isLocalIcon = () => icon?.startsWith(localPrefix)
+
+  const symbolId = () => {
+    const { VITE_ICON_LOCAL_PREFIX: prefix } = import.meta.env
+    const defaultLocalIcon = 'no-icon'
+    const iconName = icon?.replace(localPrefix, '') || defaultLocalIcon
+    return `#${prefix}-${iconName}`
+  }
+
+  return isLocalIcon() || !icon
+    ? (
+        <svg aria-hidden="true" width="1em" height="1em" {...rest}>
+          <use href={symbolId()} fill="currentColor" />
+        </svg>
+      )
+    : (
+        <Icon icon={icon} {...rest} />
+      )
+}
+
+export default SvgIcon
