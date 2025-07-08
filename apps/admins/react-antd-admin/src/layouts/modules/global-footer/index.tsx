1984e33defa2646dfc1f49ca9683ea641a3692cd
oOBobbyOo <944831575@qq.com>
2025-07-08 12:02:05 +0800

    refactor: change GlobalFooter

--- apps/admins/react-antd-admin/src/layouts/modules/global-footer/index.tsx
+++ apps/admins/react-antd-admin/src/layouts/modules/global-footer/index.tsx
@@ -3,7 +3,11 @@ import { Layout } from 'antd'
 const { Footer } = Layout
 
 function GlobalFooter() {
-  return <Footer className="global-layout-footer">GlobalFooter</Footer>
+  return (
+    <Footer className="global-layout-footer flex-center">
+      Copyright © 2025 by @henry
+    </Footer>
+  )
 }
 
 export default GlobalFooter
