4e9832af046d18d02cebe25fdbe61e6adcb27f96
oOBobbyOo <944831575@qq.com>
2025-07-10 17:56:43 +0800

    refactor: change useRouterPush

--- apps/admins/react-antd-admin/eslint.config.mjs
+++ apps/admins/react-antd-admin/eslint.config.mjs
@@ -24,6 +24,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'react-hooks/rules-of-hooks': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 )
--- apps/admins/react-antd-admin/src/hooks/useRouterPush.ts
+++ apps/admins/react-antd-admin/src/hooks/useRouterPush.ts
@@ -1,12 +1,14 @@
 import type { RouterNavigateOptions, To } from 'react-router-dom'
+import { stringifyQuery } from '@henry/utils'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 export function useRouterPush() {
   const location = useLocation()
   const navigate = useNavigate()
 
-  function push(path: To, options?: RouterNavigateOptions) {
-    navigate(path, options)
+  function push(path: To, query?: Record<string, any>, options?: RouterNavigateOptions) {
+    const _path = query ? `${path}?${stringifyQuery(query)}` : path
+    navigate(_path, options)
   }
 
   function back() {
