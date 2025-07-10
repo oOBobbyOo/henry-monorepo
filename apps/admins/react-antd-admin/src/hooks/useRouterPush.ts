15e489cb9f584e2347a2fddcb7094f7b123f2331
oOBobbyOo <944831575@qq.com>
2025-07-10 17:24:06 +0800

    feat: useRouterPush

---
+++ apps/admins/react-antd-admin/src/hooks/useRouterPush.ts
@@ -0,0 +1,47 @@
+import type { RouterNavigateOptions, To } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+export function useRouterPush() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  function push(path: To, options?: RouterNavigateOptions) {
+    navigate(path, options)
+  }
+
+  function back() {
+    navigate(-1)
+  }
+
+  function forward() {
+    navigate(1)
+  }
+
+  function go(delta: number) {
+    navigate(delta)
+  }
+
+  function replace(path: To) {
+    navigate(path, { replace: true })
+  }
+
+  function reload() {
+    navigate(0)
+  }
+
+  function toHome() {
+    navigate('/')
+  }
+
+  return {
+    location,
+    navigate,
+    push,
+    back,
+    forward,
+    go,
+    replace,
+    reload,
+    toHome,
+  }
+}
