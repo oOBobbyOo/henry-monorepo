f107cb64a9db1b8da741808b5adb9b3d08eeb504
oOBobbyOo <944831575@qq.com>
2025-07-11 10:59:54 +0800

    refactor: change GlobalContent class

--- apps/admins/react-antd-admin/src/layouts/modules/global-content/index.tsx
+++ apps/admins/react-antd-admin/src/layouts/modules/global-content/index.tsx
@@ -5,7 +5,7 @@ const { Content } = Layout
 
 function GlobalContent() {
   return (
-    <Content className="global-layout-content">
+    <Content className="global-layout-content p-4 flex-col gap-4 transition-300 overflow-y-auto">
       <Outlet />
     </Content>
   )
