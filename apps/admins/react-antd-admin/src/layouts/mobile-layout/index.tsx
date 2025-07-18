dfd28b1df479ed126ce6ed6b6f57f6c7be5e08d3
oOBobbyOo <944831575@qq.com>
2025-07-18 12:32:22 +0800

    refactor: mobile ThemeDrawer

--- apps/admins/react-antd-admin/src/layouts/mobile-layout/index.tsx
+++ apps/admins/react-antd-admin/src/layouts/mobile-layout/index.tsx
@@ -7,6 +7,7 @@ import GlobalContent from '../modules/global-content'
 import GlobalFooter from '../modules/global-footer'
 import GlobalHeader from '../modules/global-header'
 import GlobalSider from '../modules/global-sider'
+import ThemeDrawer from '../modules/theme-drawer'
 
 function MobileLayout() {
   const dispatch = useAppDispatch()
@@ -27,6 +28,7 @@ function MobileLayout() {
       </Drawer>
 
       <Layout>
+        <ThemeDrawer />
         <GlobalHeader />
         <GlobalContent />
         <GlobalFooter />
