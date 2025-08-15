7ccbfdf9482cf4fe5ea121360f5ce6d1540c5aa1
oOBobbyOo <944831575@qq.com>
2025-08-15 18:17:57 +0800

    refactor: mobile calendar

--- apps/admins/react-antd-admin/src/pages/feature/calendar/index.tsx
+++ apps/admins/react-antd-admin/src/pages/feature/calendar/index.tsx
@@ -1,14 +1,17 @@
 import type { CalendarProps } from 'antd'
 import type { Dayjs } from 'dayjs'
+import { useResponsive } from '@/hooks/useResponsive'
 import { Calendar, Card } from 'antd'
-import { Lunar } from 'lunar-typescript'
+import { Lunar, Solar } from 'lunar-typescript'
 import { useTranslation } from 'react-i18next'
 
 function CalendarDemo() {
   const { t } = useTranslation('route')
+  const { isMobile } = useResponsive()
 
   const cellRender: CalendarProps<Dayjs>['cellRender'] = (date, info) => {
     const d = Lunar.fromDate(date.toDate())
+    const s = Solar.fromDate(date.toDate())
     const ganZhi = d.getYearInGanZhi() // 干支
     const zodiac = d.getYearShengXiao() // 生肖
     const monthInChinese = `${d.getMonthInChinese()}月` // 阴历(月)
@@ -16,12 +19,24 @@ function CalendarDemo() {
     const lunar = `${monthInChinese} ${dayInChinese}` // 阴历
     const festivals = d.getFestivals() // 节日
     const solarTerm = d.getJieQi() // 节气
+    const constellation = `${s.getXingZuo()}座` // 星座
 
     if (info.type === 'month') {
       return (
         <div className="text-xs">
           <p className="flex-row justify-end flex-wrap gap-2">
             {monthInChinese}
+          </p>
+        </div>
+      )
+    }
+
+    // 移动端
+    if (isMobile) {
+      return (
+        <div className="text-xs">
+          <p className="flex-row justify-end flex-wrap gap-2">
+            <span>{dayInChinese}</span>
           </p>
         </div>
       )
@@ -42,6 +57,7 @@ function CalendarDemo() {
         </p>
         <p className="flex-row justify-end flex-wrap gap-2">
           <span>{zodiac}</span>
+          <span>{constellation}</span>
         </p>
         <p className="flex-row justify-end flex-wrap gap-2">
           <span>{solarTerm}</span>
