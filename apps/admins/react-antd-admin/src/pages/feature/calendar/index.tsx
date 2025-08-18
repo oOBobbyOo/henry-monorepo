201e480111369633b30cc98556988928c6b8c518
oOBobbyOo <944831575@qq.com>
2025-08-18 11:12:35 +0800

    refactor: change calendar

--- apps/admins/react-antd-admin/src/pages/feature/calendar/index.tsx
+++ apps/admins/react-antd-admin/src/pages/feature/calendar/index.tsx
@@ -2,12 +2,18 @@ import type { CalendarProps } from 'antd'
 import type { Dayjs } from 'dayjs'
 import { useResponsive } from '@/hooks/useResponsive'
 import { Calendar, Card } from 'antd'
+import clsx from 'clsx'
+import dayjs from 'dayjs'
 import { Lunar, Solar } from 'lunar-typescript'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 function CalendarDemo() {
   const { t } = useTranslation('route')
   const { isMobile } = useResponsive()
+
+  const [, setSelectDate] = useState<Dayjs>(() => dayjs())
+  const [panelDate, setPanelDate] = useState<Dayjs>(() => dayjs())
 
   const cellRender: CalendarProps<Dayjs>['cellRender'] = (date, info) => {
     const d = Lunar.fromDate(date.toDate())
@@ -20,6 +26,11 @@ function CalendarDemo() {
     const festivals = d.getFestivals() // 节日
     const solarTerm = d.getJieQi() // 节气
     const constellation = `${s.getXingZuo()}座` // 星座
+
+    const cellClassName = clsx({
+      'text-xs': true,
+      'text-[rgba(0,0,0,0.25)]': !panelDate.isSame(date, 'month'),
+    })
 
     if (info.type === 'month') {
       return (
@@ -34,7 +45,7 @@ function CalendarDemo() {
     // 移动端
     if (isMobile) {
       return (
-        <div className="text-xs">
+        <div className={cellClassName}>
           <p className="flex-row justify-end flex-wrap gap-2">
             <span>{dayInChinese}</span>
           </p>
@@ -43,7 +54,7 @@ function CalendarDemo() {
     }
 
     return (
-      <div className="text-xs">
+      <div className={cellClassName}>
         <p className="flex-row justify-end flex-wrap gap-2">
           <span>{ganZhi}</span>
           <span>{lunar}</span>
@@ -68,6 +79,17 @@ function CalendarDemo() {
 
   const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
     console.log(value.format('YYYY-MM-DD'), mode)
+    setPanelDate(value)
+  }
+
+  const onDateChange: CalendarProps<Dayjs>['onSelect'] = (
+    value,
+    selectInfo,
+  ) => {
+    console.log(value.format('YYYY-MM-DD'), selectInfo)
+    if (selectInfo.source === 'date') {
+      setSelectDate(value)
+    }
   }
 
   return (
@@ -77,7 +99,11 @@ function CalendarDemo() {
       variant="borderless"
       size="small"
     >
-      <Calendar cellRender={cellRender} onPanelChange={onPanelChange} />
+      <Calendar
+        cellRender={cellRender}
+        onPanelChange={onPanelChange}
+        onSelect={onDateChange}
+      />
     </Card>
   )
 }
