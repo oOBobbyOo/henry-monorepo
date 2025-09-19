98282dd02af3e871c5829f9704286a945f9e50d2
oOBobbyOo <944831575@qq.com>
2025-09-19 16:42:47 +0800

    refactor: animte DoraLoading

---
+++ apps/admins/react-antd-admin/src/components/Animate/Loading/DoraLoading.tsx
@@ -0,0 +1,11 @@
+import loading from '@/assets/loading.gif'
+
+function DoraLoading() {
+  return (
+    <div className="w-[58px] h-[58px]">
+      <img src={loading} alt="" />
+    </div>
+  )
+}
+
+export default DoraLoading
--- apps/admins/react-antd-admin/src/pages/animate/loading/index.tsx
+++ apps/admins/react-antd-admin/src/pages/animate/loading/index.tsx
@@ -1,6 +1,27 @@
+import DoraLoading from '@/components/Animate/Loading/DoraLoading'
+
+import { Grid, GridItem } from '@/components/Grid'
+
 function LoadingAnimate() {
+  const loadings = [
+    {
+      key: 'DoraLoading',
+      component: <DoraLoading />,
+    },
+  ]
+
   return (
-    <div>LoadingAnimate</div>
+    <div className="m-10">
+      <Grid gutter={[40, 60]}>
+        {loadings.map((item) => {
+          return (
+            <GridItem key={item.key} span={{ xs: 24, sm: 12, md: 8 }} className="flex-center">
+              {item.component}
+            </GridItem>
+          )
+        })}
+      </Grid>
+    </div>
   )
 }
 
