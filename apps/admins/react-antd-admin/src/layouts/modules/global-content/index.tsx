fb782ee63bef17788a42787b291ecdbfd6395f21
oOBobbyOo <944831575@qq.com>
2025-09-19 11:09:45 +0800

    refactor: change GlobalContent

--- apps/admins/react-antd-admin/src/layouts/modules/global-content/index.tsx
+++ apps/admins/react-antd-admin/src/layouts/modules/global-content/index.tsx
@@ -1,12 +1,21 @@
+import { usePreviousRoute } from '@/hooks/usePreviousRoute'
+import { useAppSelector } from '@/stores/hook'
+import { getReloadFlag } from '@/stores/modules/app/slice'
 import { Layout } from 'antd'
-import { Outlet } from 'react-router-dom'
+import { useOutlet } from 'react-router-dom'
 
 const { Content } = Layout
 
 function GlobalContent() {
+  const previousRoute = usePreviousRoute()
+
+  const currentOutlet = useOutlet(previousRoute)
+
+  const reload = useAppSelector(getReloadFlag)
+
   return (
     <Content className="global-layout-content flex-1 p-4 flex-col gap-4 transition-300 overflow-y-auto">
-      <Outlet />
+      {!reload && currentOutlet}
     </Content>
   )
 }
