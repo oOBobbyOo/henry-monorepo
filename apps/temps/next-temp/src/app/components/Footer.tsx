7190d43b0cb0123ce647f5e4e63e7c822812f744
oOBobbyOo <944831575@qq.com>
2025-11-20 17:53:33 +0800

    refactor: change app page

---
+++ apps/temps/next-temp/src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <div className="text-2xl m-5 flex justify-center gap-3">Footer</div>
+  )
+}
+
+export default Footer
--- apps/temps/next-temp/src/app/layout.tsx
+++ apps/temps/next-temp/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Footer from './components/Footer'
 import './globals.css'
 
 const geistSans = Geist({
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="px-10 py-20 text-center">
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   )
