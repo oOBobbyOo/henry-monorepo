61ec613e29ff580b4c4c3f6b36a74a477a4cbda1
oOBobbyOo <944831575@qq.com>
2025-11-20 18:35:56 +0800

    refactor: change dir

--- apps/temps/next-temp/src/app/layout.tsx
+++ apps/temps/next-temp/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
+import Footer from '@/components/Footer'
+import NavBar from '@/components/NavBar'
 import { Geist, Geist_Mono } from 'next/font/google'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar'
 import './globals.css'
 
 const geistSans = Geist({
