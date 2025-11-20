0a33071e67c85b3e124d756fdb1c603bda354505
oOBobbyOo <944831575@qq.com>
2025-11-20 19:11:24 +0800

    refactor: change name page

--- apps/temps/next-temp/src/app/hi/[name]/page.tsx
+++ apps/temps/next-temp/src/app/hi/[name]/page.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import { FaHandsClapping } from 'react-icons/fa6'
+
 interface Props {
   name: string
 }
@@ -7,8 +10,20 @@ async function page({ params }: { params: Promise<Props> }) {
 
   return (
     <div>
-      name:
-      {name}
+      <div className="text-yellow-300 text-4xl inline-block animate-shake-x duration-5000">
+        <FaHandsClapping />
+      </div>
+      <h3 className="py-2 text-2xl font-500">Hi,</h3>
+      <div className="text-xl">
+        {name}
+        {' '}
+        :）
+      </div>
+      <div>
+        <Link className="btn text-sm m-3" href="/">
+          Back
+        </Link>
+      </div>
     </div>
   )
 }
