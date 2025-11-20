68dcf201734fc7107c9e7a65ad2b8556d74317eb
oOBobbyOo <944831575@qq.com>
2025-11-20 18:08:50 +0800

    refactor: change Footer

--- apps/temps/next-temp/src/app/components/Footer.tsx
+++ apps/temps/next-temp/src/app/components/Footer.tsx
@@ -1,6 +1,40 @@
+import Link from 'next/link'
+import { FaGithub, FaHome } from 'react-icons/fa'
+import { MdLibraryBooks } from 'react-icons/md'
+
 function Footer() {
+  const links = [
+    {
+      label: 'Home',
+      icon: <FaHome />,
+      to: '/',
+    },
+    {
+      label: 'Next',
+      icon: <MdLibraryBooks />,
+      to: 'https://nextjs.org/docs',
+    },
+    {
+      label: 'GitHub',
+      icon: <FaGithub />,
+      to: 'https://github.com/oOBobbyOo/henry-monorepo/tree/main/apps/temps/next-temp',
+    },
+  ]
+
   return (
-    <div className="text-2xl m-5 flex justify-center gap-3">Footer</div>
+    <div className="text-2xl m-5 flex justify-center gap-4">
+      {links.map((item, i) => {
+        return (
+          <Link
+            href={item.to}
+            key={i}
+            target={item.to.startsWith('http') ? '_blank' : undefined}
+          >
+            {item.icon}
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
