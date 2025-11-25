153add3fd484bb5f43dafac4c6205d6e5806ab0a
oOBobbyOo <944831575@qq.com>
2025-11-25 15:42:28 +0800

    refactor: update unocss version

---
+++ apps/temps/nuxt-temp/app/pages/hi/[name].vue
@@ -0,0 +1,30 @@
+<script setup lang='ts'>
+definePageMeta({
+  layout: 'home',
+})
+
+const route = useRoute<'hi-name'>()
+
+const name = route.params.name
+</script>
+
+<template>
+  <div>
+    <div i-twemoji:waving-hand text-4xl inline-block animate-shake-x animate-duration-5000 />
+    <h3 text-2xl font-500>
+      Hi,
+    </h3>
+    <div text-xl>
+      {{ name }}!
+    </div>
+
+    <div>
+      <NuxtLink
+        class="text-sm btn m-3"
+        to="/"
+      >
+        Back
+      </NuxtLink>
+    </div>
+  </div>
+</template>
