306661a92087fcadaffbce34bde2fa8f31222c20
oOBobbyOo <944831575@qq.com>
2025-11-27 18:48:50 +0800

    refactor: add error page

---
+++ apps/temps/nuxt-temp/app/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+  <div size-full flex="~ col gap3" justify-center items-center>
+    <h1>{{ error.statusCode }}</h1>
+
+    <NuxtLink to="/" class="btn text-sm">
+      Go back
+    </NuxtLink>
+  </div>
+</template>
